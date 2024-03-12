fb7233760c25318d80a7e53a03235f7dedb5d412
Miguel Lo-A-Foe <miguel@loafoe.dev>
2024-03-12 11:57:49 +0100

    add ci-file to test

--- RUNME.sh
+++ RUNME.sh
@@ -1,9 +1,11 @@
 #! /bin/sh
 
-export PATH="$(python3 -c 'import os,sysconfig;print(sysconfig.get_path("scripts",f"{os.name}_user"))'):$PATH"
+PYTHON_PATH=$(python3 -c 'import os,sysconfig;print(sysconfig.get_path("scripts",f"{os.name}_user"))')
+PATH="$PYTHON_PATH:$PATH"
+export PATH
 
-python3 -m pip install --upgrade --user pip
-python3 -m pip install --user ansible-core
+python3 -m pip install --quiet --upgrade --user pip
+python3 -m pip install --quiet --user ansible-core
 
 ansible-galaxy install -r requirements.yml
 ansible-playbook playbook.yaml
