222b26cb73fc0fde3072ace3611038422ffdb231
Miguel Lo-A-Foe <miguel@loafoe.dev>
2024-03-13 22:23:28 +0100

    update runme

--- RUNME.sh
+++ RUNME.sh
@@ -8,4 +8,4 @@ python3 -m pip install --quiet --upgrade pip
 python3 -m pip install --quiet ansible-core
 
 ansible-galaxy install -r requirements.yml
-ansible-playbook playbook.yaml
+ansible-playbook playbook.yaml --ask-become-pass
