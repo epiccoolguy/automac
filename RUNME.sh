ca0f0e038472f7b5645c4e1e0b63afc267bccd95
Miguel Lo-A-Foe <miguel@loafoe.dev>
2024-03-13 18:42:57 +0100

    add venv to RUNME

--- RUNME.sh
+++ RUNME.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
 
-PYTHON_PATH=$(python3 -c 'import os,sysconfig;print(sysconfig.get_path("scripts",f"{os.name}_user"))')
-PATH="$PYTHON_PATH:$PATH"
-export PATH
+TEMP_DIR=$(mktemp -d)
+python3 -m venv "$TEMP_DIR"
+. "$TEMP_DIR/bin/activate"
 
-python3 -m pip install --quiet --upgrade --user pip
-python3 -m pip install --quiet --user ansible-core
+python3 -m pip install --quiet --upgrade pip
+python3 -m pip install --quiet ansible-core
 
 ansible-galaxy install -r requirements.yml
 ansible-playbook playbook.yaml
